d1e1559a5fd55444bcd8ea25c2fe3aa3f4f54ed4
alaa <alaa1996_2013@hotmail.com>
2023-07-07 21:14:18 +0300

    login & logout

---
+++ routes/backend.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Http\Controllers\Dashboard\DashboardController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard/user', function () {
+    return view('Dashboard.User.dashboard');
+})->middleware(['auth'])->name('dashboard.user');
+
+
+Route::get('/dashboard/admin', function () {
+    return view('Dashboard.Admin.dashboard');
+})->middleware(['auth:admin'])->name('dashboard.admin');
+
+
+require __DIR__.'/auth.php';
